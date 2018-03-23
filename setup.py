c65c6da8de768c9e896a51f1e79cfa1b537ad5fd
Mario Geiger <geiger.mario@gmail.com>
2018-03-23 11:48:52 +0100

    simplify setup.py

--- setup.py
+++ setup.py
@@ -2,36 +2,26 @@
 import os
 from setuptools import setup, find_packages
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-this_dir = os.path.dirname(__file__)
-
-cffi_modules = []
-
 try:
     import torch
-    if torch.cuda.is_available():
-        print("try to build CUDA depdendencies")
-        cffi_modules.append(os.path.join(this_dir, "build.py:ffi_plan_cufft"))
-    else:
-        print("CUDA is not available on your system.")
+    if not torch.cuda.is_available():
+        print("torch.cuda.is_available() returned False")
+        exit()
 except ImportError:
-    print("PyTorch is not available on your system.")
-
-print(find_packages(exclude=["build"]))
+    print("Cannot import torch")
+    exit()
 
 setup(
     name='s2cnn',
-    version = "1.0.0",
-    author = "Mario Geiger, Taco Cohen, Jonas Koehler",
-    description = ("SO(3) equivariant CNNs for PyTorch."),
-    license = "MIT",
-    keywords = "so3 equivariant cnn pytorch",
-    url = "https://github.com/AMLab-Amsterdam/s2cnn",
-    long_description=read('README.md'),
+    version="1.0.0",
+    author="Mario Geiger, Taco Cohen, Jonas Koehler",
+    description=("SO(3) equivariant CNNs for PyTorch."),
+    license="MIT",
+    keywords="so3 equivariant cnn pytorch",
+    url="https://github.com/AMLab-Amsterdam/s2cnn",
+    long_description=open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
     install_requires=["cffi>=1.0.0"],
     setup_requires=["cffi>=1.0.0"],
     packages=find_packages(exclude=["build"]),
-    cffi_modules=cffi_modules,
+    cffi_modules=[os.path.join(os.path.dirname(__file__), "build.py:ffi_plan_cufft")],
 )
